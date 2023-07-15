c70e4b3312d8ea51a7b7b41510ba2a0a47eb065b
李君祥 <2952434583@qq.com>
2023-07-15 21:53:44 +0800

    吸顶导航交互实现

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,12 +2,14 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
 import '@/styles/common.scss'
 </script>
 
 <template>
-  <LayoutNav />
-  <LayoutHeader />
-  <RouterView />
-  <LayoutFooter />
+  <LayoutFixed/>
+  <LayoutNav/>
+  <LayoutHeader/>
+  <RouterView/>
+  <LayoutFooter/>
 </template>
