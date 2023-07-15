75cdb97db6bfb31ce15fae6ff7ea39bbb7b906bc
李君祥 <2952434583@qq.com>
2023-07-15 23:36:15 +0800

    home-新鲜好物模块实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -4,8 +4,18 @@ import httpInstance from "@/utils/http";
  * 获取轮播图图片
  * @returns {*}
  */
-export function getBannerAPI() {
+export const getBannerAPI = () => {
     return httpInstance({
         url: 'home/banner'
+    })
+}
+
+/**
+ * 获取新鲜好物
+ * @returns {*}
+ */
+export const findNewAPI = () => {
+    return httpInstance({
+        url: '/home/new'
     })
 }
