84776d855b25eaf6a727199b9de08d9938230a21
李君祥 <2952434583@qq.com>
2023-07-16 22:35:54 +0800

    二级分类-列表筛选实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -29,12 +29,16 @@ const reqData = ref({
 })
 const getGoodList = async () => {
   const res = await getSubCategoryAPI(reqData.value)
-  console.log(res)
-  console.log(res.result)
   goodList.value = res.result.items
 }
 
 onMounted(() => getGoodList())
+
+// tab切换回调
+const tabChange = () => {
+  reqData.value.page = 1
+  getGoodList()
+}
 
 </script>
 
@@ -50,7 +54,7 @@ onMounted(() => getGoodList())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
