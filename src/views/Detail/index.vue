a062151b32032b63b2730eebeee93b40f0ad7e37
李君祥 <2952434583@qq.com>
2023-07-20 21:42:56 +0800

    热榜基础组件封装和基础数据获取渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,7 @@
 import {getDetail} from "@/apis/detail";
 import {ref, onMounted} from "vue";
 import {useRoute} from "vue-router";
+import DetailHot from './composables/DetailHot.vue'
 
 const goods = ref({})
 
@@ -116,7 +117,8 @@ onMounted(() => getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+              <DetailHot/>
+              <DetailHot/>
             </div>
           </div>
         </div>
