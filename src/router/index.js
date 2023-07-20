cc23ecfd271dfe4b726181eff68b5f8563145913
李君祥 <2952434583@qq.com>
2023-07-20 21:15:01 +0800

    整体认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,50 @@
 // createRouter: 创建router实例对象
 // createWebHistory: 创建history模式的路由
 
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  // path和component的对应关系
-  routes: [
-    {
-      path: '/',
-      component: Layout,
-      children: [
+    history: createWebHistory(import.meta.env.BASE_URL),
+    // path和component的对应关系
+    routes: [
         {
-          path: '',
-          component: Home
+            path: '/',
+            component: Layout,
+            children: [
+                {
+                    path: '',
+                    component: Home
+                },
+                {
+                    path: 'category/:id',
+                    component: Category
+                },
+                {
+                    path: 'category/sub/:id',
+                    component: SubCategory
+                },
+                {
+                    path: 'detail/:id',
+                    component: Detail
+                }
+            ]
         },
         {
-          path: 'category/:id',
-          component: Category
-        },
-        {
-          path: 'category/sub/:id',
-          component: SubCategory
+            path: '/login',
+            component: Login
         }
-      ]
-    },
-    {
-      path: '/login',
-      component: Login
+    ],
+    scrollBehavior() {
+        return {
+            top: 0
+        }
     }
-  ],
-  scrollBehavior() {
-    return {
-      top: 0
-    }
-  }
 })
 
 export default router
