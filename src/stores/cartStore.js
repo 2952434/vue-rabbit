63e0771dbf17492c7920a328cb7a511bfc6e41d4
李君祥 <2952434583@qq.com>
2023-07-26 22:46:21 +0800

    列表购物车-全选功能实现

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -43,13 +43,25 @@ export const useCartStore = defineStore('cart', () => {
         item.selected = selected
     }
 
+
+    // 全选功能action
+    const allCheck = (selected) => {
+        // 把cartList中的每一项的selected都设置为当前的全选框状态
+        cartList.value.forEach(item => item.selected = selected)
+    }
+
+    // 是否全选计算属性
+    const isAll = computed(() => cartList.value.every((item) => item.selected))
+
     return {
         cartList,
         allCount,
         allPrice,
+        isAll,
         addCart,
         delCart,
-        singleCheck
+        singleCheck,
+        allCheck
     }
 
 }, {
