3bb4d2184728bcc1c2c460845903060317000723
李君祥 <2952434583@qq.com>
2023-07-26 21:45:15 +0800

    购物车—本地添加购物车

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -5,6 +5,10 @@ import {useRoute} from "vue-router";
 import DetailHot from './components/DetailHot.vue'
 // import ImageView from '@/components/iamgeview/index.vue'
 // import XtxSku from '@/components/XtxSku/index.vue'
+import {ElMessage} from "element-plus";
+import {useCartStore} from "@/stores/cartStore";
+
+const cartStore = useCartStore()
 
 const goods = ref({})
 
@@ -17,8 +21,37 @@ const getGoods = async () => {
 onMounted(() => getGoods())
 
 // sku规格操作时
+let skuObj = {}
 const skuChange = (sku) => {
-  console.log(sku)
+  // console.log(sku)
+  skuObj = sku
+}
+
+// 购物车逻辑
+// count
+const count = ref(1)
+const countChange = (count) => {
+  console.log(count)
+}
+
+// 添加购物车
+const addCart = () => {
+
+  if (skuObj.skuId) {
+    console.log(skuObj.skuId)
+    cartStore.addCart({
+      id: goods.value.id,
+      name: goods.value.name,
+      picture: goods.value.mainPictures[0],
+      price: goods.value.price,
+      skuId: skuObj.skuId,
+      attrsText: skuObj.specsText,
+      selected: true
+    })
+  }else {
+    // 规格没有选择 提醒用户
+    ElMessage.warning('请选择规格')
+  }
 }
 
 </script>
@@ -93,10 +126,10 @@ const skuChange = (sku) => {
               <!-- sku组件 -->
               <XtxSku :goods="goods" @change="skuChange"/>
               <!-- 数据组件 -->
-
+              <el-input-number v-model="count" @change="countChange"/>
               <!-- 按钮组件 -->
               <div>
-                <el-button size="large" class="btn">
+                <el-button size="large" class="btn" @click="addCart">
                   加入购物车
                 </el-button>
               </div>
